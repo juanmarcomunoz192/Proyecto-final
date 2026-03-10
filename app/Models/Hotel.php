867fba3098a1e7da4b3063393cc1b8cd89523225
juan <juanmarcomunoz192@gmail.com>
2026-03-10 20:41:49 +0100

    codigo controladores hoteles

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -10,6 +10,7 @@ class Hotel extends Model
     use HasFactory;
     protected $table = 'hoteles';
     public $timestamps = true;
+    protected $fillable = ['nombre', 'direccion', 'ciudad'];
     public function habitaciones()
     {
         return $this->hasMany(Habitacion::class);
